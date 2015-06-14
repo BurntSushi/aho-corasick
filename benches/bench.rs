17a9ce001315719c98b1fc49ea00b0367fa03376
Andrew Gallant <jamslam@gmail.com>
2015-06-14 02:30:07 -0400

    Various improvements, tests and benchmarks.
    Still not quite polished yet.

--- benches/bench.rs
+++ benches/bench.rs
@@ -5,110 +5,223 @@ extern crate test;
 
 use std::iter;
 
-use aho_corasick::{Builder};
+use aho_corasick::AcAutomaton;
 use test::Bencher;
+
+const HAYSTACK_RANDOM: &'static str = include_str!("random.txt");
+
+fn bench_aut_no_match(b: &mut Bencher, aut: AcAutomaton, haystack: &str) {
+    b.bytes = haystack.len() as u64;
+    b.iter(|| assert!(aut.find(haystack).next().is_none()));
+}
+
+fn bench_naive_no_match<S>(b: &mut Bencher, needles: Vec<S>, haystack: &str)
+        where S: Into<String> {
+    b.bytes = haystack.len() as u64;
+    let needles: Vec<String> = needles.into_iter().map(Into::into).collect();
+    b.iter(|| assert!(!naive_find(&needles, haystack)));
+}
+
+fn haystack_same(letter: char) -> String {
+    iter::repeat(letter).take(10000).collect()
+}
 
 #[bench]
 fn one_byte(b: &mut Bencher) {
-    let haystack: String = iter::repeat("z").take(10000).collect();
-    let aut = Builder::new().add("a").build();
-    b.iter(|| {
-        assert!(aut.find(&haystack).is_empty());
-    });
-}
-
-/*
-#[bench]
-fn one(b: &mut Bencher) {
-    let haystack: String = iter::repeat("z").take(10000).collect();
-    let haystack = haystack.as_bytes();
-    let needle = b"abc";
-    b.iter(|| {
-        assert!(prefix::find_one(needle, haystack).is_none());
-    });
+    let aut = AcAutomaton::new(vec!["a"]);
+    bench_aut_no_match(b, aut, &haystack_same('z'));
 }
 
 #[bench]
-fn one_tricky(b: &mut Bencher) {
+fn one_prefix_byte_no_match(b: &mut Bencher) {
+    let aut = AcAutomaton::new(vec!["zbc"]);
+    bench_aut_no_match(b, aut, &haystack_same('y'));
+}
+
+#[bench]
+fn one_prefix_byte_every_match(b: &mut Bencher) {
     // We lose the benefit of `memchr` because the first byte matches
     // in every position in the haystack.
-    let haystack: String = iter::repeat("z").take(10000).collect();
-    let haystack = haystack.as_bytes();
-    let needle = b"zbc";
-    b.iter(|| {
-        assert!(prefix::find_one(needle, haystack).is_none());
-    });
+    let aut = AcAutomaton::new(vec!["zbc"]);
+    bench_aut_no_match(b, aut, &haystack_same('z'));
+}
+
+#[bench]
+fn one_prefix_byte_random(b: &mut Bencher) {
+    let aut = AcAutomaton::new(vec!["zbc\x00"]);
+    bench_aut_no_match(b, aut, HAYSTACK_RANDOM);
 }
 
 #[bench]
 fn two_bytes(b: &mut Bencher) {
-    let haystack: String = iter::repeat("z").take(10000).collect();
-    let haystack = haystack.as_bytes();
-    let needles = &["a", "b"];
-    let needles: Vec<String> = needles.iter().map(|&s| s.to_owned()).collect();
-    b.iter(|| {
-        assert!(prefix::find_any(&needles, haystack).is_none());
-    });
+    let aut = AcAutomaton::new(vec!["a", "b"]);
+    bench_aut_no_match(b, aut, &haystack_same('z'));
 }
 
 #[bench]
-fn two(b: &mut Bencher) {
-    let haystack: String = iter::repeat("z").take(10000).collect();
-    let haystack = haystack.as_bytes();
-    let needles = &["abcdef", "bmnopq"];
-    let needles: Vec<String> = needles.iter().map(|&s| s.to_owned()).collect();
-    b.iter(|| {
-        assert!(prefix::find_any(&needles, haystack).is_none());
-    });
+fn two_diff_prefix(b: &mut Bencher) {
+    let aut = AcAutomaton::new(vec!["abcdef", "bmnopq"]);
+    bench_aut_no_match(b, aut, &haystack_same('z'));
 }
 
 #[bench]
-fn two_tricky(b: &mut Bencher) {
-    let haystack: String = iter::repeat("z").take(10000).collect();
-    let haystack = haystack.as_bytes();
-    let needles = &["zbcdef", "zmnopq"];
-    let needles: Vec<String> = needles.iter().map(|&s| s.to_owned()).collect();
-    b.iter(|| {
-        assert!(prefix::find_any(&needles, haystack).is_none());
-    });
+fn two_one_prefix_byte_every_match(b: &mut Bencher) {
+    let aut = AcAutomaton::new(vec!["zbcdef", "zmnopq"]);
+    bench_aut_no_match(b, aut, &haystack_same('z'));
+}
+
+#[bench]
+fn two_one_prefix_byte_no_match(b: &mut Bencher) {
+    let aut = AcAutomaton::new(vec!["zbcdef", "zmnopq"]);
+    bench_aut_no_match(b, aut, &haystack_same('y'));
+}
+
+#[bench]
+fn two_one_prefix_byte_random(b: &mut Bencher) {
+    let aut = AcAutomaton::new(vec!["zbcdef\x00", "zmnopq\x00"]);
+    bench_aut_no_match(b, aut, HAYSTACK_RANDOM);
 }
 
 #[bench]
 fn ten_bytes(b: &mut Bencher) {
-    let haystack: String = iter::repeat("z").take(10000).collect();
-    let haystack = haystack.as_bytes();
-    let needles = &["a", "b", "c", "d", "e",
-                    "f", "g", "h", "i", "j"];
-    let needles: Vec<String> = needles.iter().map(|&s| s.to_owned()).collect();
-    b.iter(|| {
-        assert!(prefix::find_any(&needles, haystack).is_none());
-    });
+    let aut = AcAutomaton::new(vec!["a", "b", "c", "d", "e",
+                                    "f", "g", "h", "i", "j"]);
+    bench_aut_no_match(b, aut, &haystack_same('z'));
 }
 
 #[bench]
-fn ten(b: &mut Bencher) {
-    let haystack: String = iter::repeat("z").take(10000).collect();
-    let haystack = haystack.as_bytes();
-    let needles = &["abcdef", "bbcdef", "cbcdef", "dbcdef",
-                    "ebcdef", "fbcdef", "gbcdef", "hbcdef",
-                    "ibcdef", "jbcdef"];
-    let needles: Vec<String> = needles.iter().map(|&s| s.to_owned()).collect();
-    b.iter(|| {
-        assert!(prefix::find_any(&needles, haystack).is_none());
-    });
+fn ten_diff_prefix(b: &mut Bencher) {
+    let aut = AcAutomaton::new(vec!["abcdef", "bbcdef", "cbcdef", "dbcdef",
+                                    "ebcdef", "fbcdef", "gbcdef", "hbcdef",
+                                    "ibcdef", "jbcdef"]);
+    bench_aut_no_match(b, aut, &haystack_same('z'));
 }
-*/
 
 #[bench]
-fn ten_tricky(b: &mut Bencher) {
-    let haystack: String = iter::repeat("z").take(10000).collect();
-    let needles = &["zacdef", "zbcdef", "zccdef", "zdcdef",
-                    "zecdef", "zfcdef", "zgcdef", "zhcdef",
-                    "zicdef", "zjcdef"];
-    let mut baut = Builder::new();
-    for &needle in needles { baut = baut.add(needle); }
-    let aut = baut.build();
-    b.iter(|| {
-        assert!(aut.find(&haystack).is_empty());
-    });
+fn ten_one_prefix_byte_every_match(b: &mut Bencher) {
+    let aut = AcAutomaton::new(vec!["zacdef", "zbcdef", "zccdef", "zdcdef",
+                                    "zecdef", "zfcdef", "zgcdef", "zhcdef",
+                                    "zicdef", "zjcdef"]);
+    bench_aut_no_match(b, aut, &haystack_same('z'));
+}
+
+#[bench]
+fn ten_one_prefix_byte_no_match(b: &mut Bencher) {
+    let aut = AcAutomaton::new(vec!["zacdef", "zbcdef", "zccdef", "zdcdef",
+                                    "zecdef", "zfcdef", "zgcdef", "zhcdef",
+                                    "zicdef", "zjcdef"]);
+    bench_aut_no_match(b, aut, &haystack_same('y'));
+}
+
+#[bench]
+fn ten_one_prefix_byte_random(b: &mut Bencher) {
+    let aut = AcAutomaton::new(vec!["zacdef\x00", "zbcdef\x00", "zccdef\x00",
+                                    "zdcdef\x00", "zecdef\x00", "zfcdef\x00",
+                                    "zgcdef\x00", "zhcdef\x00", "zicdef\x00",
+                                    "zjcdef\x00"]);
+    bench_aut_no_match(b, aut, HAYSTACK_RANDOM);
+}
+
+// A naive multi-pattern search.
+// We use this to benchmark *throughput*, so it should never match anything.
+fn naive_find(needles: &[String], haystack: &str) -> bool {
+    for hi in 0..haystack.len() {
+        let rest = &haystack.as_bytes()[hi..];
+        for needle in needles {
+            let needle = needle.as_bytes();
+            if needle.len() > rest.len() {
+                continue;
+            }
+            if needle == &rest[..needle.len()] {
+                // should never happen in throughput benchmarks.
+                return true;
+            }
+        }
+    }
+    false
+}
+
+#[bench]
+fn naive_one_byte(b: &mut Bencher) {
+    bench_naive_no_match(b, vec!["a"], &haystack_same('z'));
+}
+
+#[bench]
+fn naive_one_prefix_byte_no_match(b: &mut Bencher) {
+    bench_naive_no_match(b, vec!["zbc"], &haystack_same('y'));
+}
+
+#[bench]
+fn naive_one_prefix_byte_every_match(b: &mut Bencher) {
+    bench_naive_no_match(b, vec!["zbc"], &haystack_same('z'));
+}
+
+#[bench]
+fn naive_one_prefix_byte_random(b: &mut Bencher) {
+    bench_naive_no_match(b, vec!["zbc\x00"], HAYSTACK_RANDOM);
+}
+
+#[bench]
+fn naive_two_bytes(b: &mut Bencher) {
+    bench_naive_no_match(b, vec!["a", "b"], &haystack_same('z'));
+}
+
+#[bench]
+fn naive_two_diff_prefix(b: &mut Bencher) {
+    bench_naive_no_match(b, vec!["abcdef", "bmnopq"], &haystack_same('z'));
+}
+
+#[bench]
+fn naive_two_one_prefix_byte_every_match(b: &mut Bencher) {
+    bench_naive_no_match(b, vec!["zbcdef", "zmnopq"], &haystack_same('z'));
+}
+
+#[bench]
+fn naive_two_one_prefix_byte_no_match(b: &mut Bencher) {
+    bench_naive_no_match(b, vec!["zbcdef", "zmnopq"], &haystack_same('y'));
+}
+
+#[bench]
+fn naive_two_one_prefix_byte_random(b: &mut Bencher) {
+    bench_naive_no_match(b, vec!["zbcdef\x00", "zmnopq\x00"], HAYSTACK_RANDOM);
+}
+
+#[bench]
+fn naive_ten_bytes(b: &mut Bencher) {
+    let needles = vec!["a", "b", "c", "d", "e",
+                       "f", "g", "h", "i", "j"];
+    bench_naive_no_match(b, needles, &haystack_same('z'));
+}
+
+#[bench]
+fn naive_ten_diff_prefix(b: &mut Bencher) {
+    let needles = vec!["abcdef", "bbcdef", "cbcdef", "dbcdef",
+                       "ebcdef", "fbcdef", "gbcdef", "hbcdef",
+                       "ibcdef", "jbcdef"];
+    bench_naive_no_match(b, needles, &haystack_same('z'));
+}
+
+#[bench]
+fn naive_ten_one_prefix_byte_every_match(b: &mut Bencher) {
+    let needles = vec!["zacdef", "zbcdef", "zccdef", "zdcdef",
+                       "zecdef", "zfcdef", "zgcdef", "zhcdef",
+                       "zicdef", "zjcdef"];
+    bench_naive_no_match(b, needles, &haystack_same('z'));
+}
+
+#[bench]
+fn naive_ten_one_prefix_byte_no_match(b: &mut Bencher) {
+    let needles = vec!["zacdef", "zbcdef", "zccdef", "zdcdef",
+                       "zecdef", "zfcdef", "zgcdef", "zhcdef",
+                       "zicdef", "zjcdef"];
+    bench_naive_no_match(b, needles, &haystack_same('y'));
+}
+
+#[bench]
+fn naive_ten_one_prefix_byte_random(b: &mut Bencher) {
+    let needles = vec!["zacdef\x00", "zbcdef\x00", "zccdef\x00",
+                       "zdcdef\x00", "zecdef\x00", "zfcdef\x00",
+                       "zgcdef\x00", "zhcdef\x00", "zicdef\x00",
+                       "zjcdef\x00"];
+    bench_naive_no_match(b, needles, HAYSTACK_RANDOM);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,36 +4,25 @@ A fast implementation of the Aho-Corasick string search algorithm.
 
 #![allow(dead_code)]
 
+extern crate memchr;
+
 use std::collections::VecDeque;
 use std::fmt;
+use std::iter::FromIterator;
 
-#[derive(Clone, Debug)]
-pub struct Builder {
-    pats: Vec<String>,
-}
-
-impl Builder {
-    pub fn new() -> Builder {
-        Builder { pats: vec![] }
-    }
-
-    pub fn add<S: Into<String>>(mut self, s: S) -> Builder {
-        self.pats.push(s.into());
-        self
-    }
-
-    pub fn build(self) -> Automaton {
-        Automaton::new(self.pats)
-    }
-}
+use memchr::memchr;
 
 type PatIdx = usize;
-type StateIdx = usize;
+type StateIdx = u32;
+
+const FAIL_STATE: u32 = 0;
+const ROOT_STATE: u32 = 1;
 
 #[derive(Clone)]
-pub struct Automaton {
+pub struct AcAutomaton {
     pats: Vec<String>,
     states: Vec<State>,
+    start_bytes: Vec<u8>,
 }
 
 #[derive(Clone)]
@@ -43,56 +32,219 @@ struct State {
     goto: Vec<StateIdx>, // indexed by alphabet
 }
 
-impl Automaton {
-    fn new(pats: Vec<String>) -> Automaton {
-        Automaton {
+impl AcAutomaton {
+    pub fn new<S, I>(pats: I) -> AcAutomaton
+            where S: Into<String>, I: IntoIterator<Item=S> {
+        AcAutomaton {
             pats: vec![], // filled in later, avoid wrath of borrow checker
-            states: vec![State::new()],
-        }.build(pats)
+            states: vec![State::new(), State::new()], // empty and root
+            start_bytes: vec![], // also filled in later
+        }.build(pats.into_iter().map(Into::into).collect())
     }
 
-    fn build(mut self, pats: Vec<String>) -> Automaton {
-        let rooti = self.add_state(State::new());
+    pub fn len(&self) -> usize {
+        self.pats.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    pub fn find<'a, 's>(&'a self, s: &'s str) -> Matches<'a, 's> {
+        Matches {
+            aut: self,
+            text: s.as_bytes(),
+            texti: 0,
+            si: ROOT_STATE,
+            outi: 0,
+            overlapping: false,
+        }
+    }
+
+    pub fn find_overlapping<'a, 's>(&'a self, s: &'s str) -> Matches<'a, 's> {
+        Matches {
+            aut: self,
+            text: s.as_bytes(),
+            texti: 0,
+            si: ROOT_STATE,
+            outi: 0,
+            overlapping: true,
+        }
+    }
+
+    pub fn pattern(&self, i: usize) -> &str {
+        &self.pats[i]
+    }
+
+    fn next_state(&self, mut si: StateIdx, b: u8) -> StateIdx {
+        // Eliding bounds checking here increases throughput by about 18%
+        // on the `*_every_match` benchmarks.
+        // For the `*_random` benchmarks, it's more like 5%.
+        // And for the `*_no_match` benchmarks, it's more like 0.5%.
+        //
+        // 5% on the average ("random") case seems worth it. Improving the
+        // absolute worst case ("every match") by 18% is nice too, I guess.
+        loop {
+            let maybe_si = unsafe {
+                *self.states.get_unchecked(si as usize)
+                     .goto.get_unchecked(b as usize)
+            };
+            if maybe_si != FAIL_STATE {
+                si = maybe_si;
+                break;
+            } else {
+                si = unsafe { self.states.get_unchecked(si as usize).fail };
+            }
+        }
+        si
+    }
+}
+
+#[derive(Debug)]
+pub struct Matches<'a, 's> {
+    aut: &'a AcAutomaton,
+    text: &'s [u8],
+    texti: usize,
+    si: StateIdx,
+    outi: usize,
+    overlapping: bool,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Match {
+    pub pati: usize,
+    pub start: usize,
+    pub end: usize,
+}
+
+impl<'a, 's> Iterator for Matches<'a, 's> {
+    type Item = Match;
+
+    fn next(&mut self) -> Option<Match> {
+        if self.has_match() {
+            let m = self.get_match();
+            self.outi += 1;
+            if !self.has_match() {
+                self.texti += 1;
+            }
+            return Some(m);
+        }
+        self.outi = 0;
+
+        // If there is a common single byte prefix, then it's worth it to try
+        // to skip along the input with `memchr`.
+        //
+        // If not, then the `skip`s hurt performance pretty bad (possibly
+        // harming the branch predictor), so move the case analysis to every
+        // match.
+        if self.aut.start_bytes.len() == 1 {
+            if self.si == ROOT_STATE {
+                self.skip();
+            }
+            while self.texti < self.text.len() {
+                let b = self.text[self.texti];
+                self.si = self.aut.next_state(self.si, b);
+                if self.has_match() {
+                    return self.next();
+                }
+                self.texti += 1;
+                if self.si == ROOT_STATE {
+                    self.skip();
+                }
+            }
+        } else {
+            while self.texti < self.text.len() {
+                let b = self.text[self.texti];
+                self.si = self.aut.next_state(self.si, b);
+                if self.has_match() {
+                    return self.next();
+                }
+                self.texti += 1;
+            }
+        }
+        None
+    }
+}
+
+impl<'a, 's> Matches<'a, 's> {
+    fn skip(&mut self) {
+        let b = self.aut.start_bytes[0];
+        self.texti = match memchr(b, &self.text[self.texti..]) {
+            Some(i) => self.texti + i,
+            None => self.text.len(),
+        };
+    }
+
+    fn has_match(&self) -> bool {
+        // Eliding the bounds checking here seems to have very similar perf
+        // characteristics as eliding bounds checking in `next_state`.
+        // (See comments in `next_state`.)
+        let outs = unsafe {
+            self.aut.states.get_unchecked(self.si as usize).out.len()
+        };
+        self.outi < outs
+        // (self.overlapping && self.outi < outs)
+        // || (!self.overlapping && self.outi == 0 && outs > 0)
+    }
+
+    fn get_match(&self) -> Match {
+        let pati = self.aut.states[self.si as usize].out[self.outi];
+        let patlen = self.aut.pats[pati].len();
+        let start = self.texti + 1 - patlen;
+        Match {
+            pati: pati,
+            start: start,
+            end: start + patlen,
+        }
+    }
+}
+
+impl AcAutomaton {
+    fn build(mut self, pats: Vec<String>) -> AcAutomaton {
         for (pati, pat) in pats.iter().enumerate() {
-            let mut previ = rooti;
+            let mut previ = ROOT_STATE;
             for &b in pat.as_bytes() {
-                if let Some(si) = self.states[previ].goto(b) {
-                    previ = si;
+                if self.states[previ as usize].goto[b as usize] != FAIL_STATE {
+                    previ = self.states[previ as usize].goto[b as usize];
                 } else {
                     let nexti = self.add_state(State::new());
-                    self.states[previ].goto[b as usize] = nexti;
+                    self.states[previ as usize].goto[b as usize] = nexti;
                     previ = nexti;
                 }
             }
-            self.states[previ].out.push(pati);
+            self.states[previ as usize].out.push(pati);
         }
-        for v in &mut self.states[rooti].goto {
-            if *v == 0 {
-                *v = 1;
+        for c in 0..256 {
+            if self.states[ROOT_STATE as usize].goto[c] == FAIL_STATE {
+                self.states[ROOT_STATE as usize].goto[c] = ROOT_STATE;
+            } else {
+                self.start_bytes.push(c as u8);
             }
         }
         self.pats = pats;
         self.fill()
     }
 
-    fn fill(mut self) -> Automaton {
+    fn fill(mut self) -> AcAutomaton {
         let mut q = VecDeque::new();
-        for &si in self.states[1].goto.iter().filter(|&&si| si != 1) {
-            q.push_front(si);
+        for &si in self.states[ROOT_STATE as usize].goto.iter() {
+            if si != ROOT_STATE {
+                q.push_front(si);
+            }
         }
         while let Some(si) = q.pop_back() {
             for c in 0..256 {
-                let u = self.states[si].goto[c];
-                if u != 0 {
+                let u = self.states[si as usize].goto[c];
+                if u != FAIL_STATE {
                     q.push_front(u);
-                    let mut v = self.states[si].fail;
-                    while self.states[v].goto[c] == 0 {
-                        v = self.states[v].fail;
+                    let mut v = self.states[si as usize].fail;
+                    while self.states[v as usize].goto[c] == FAIL_STATE {
+                        v = self.states[v as usize].fail;
                     }
-                    let ufail = self.states[v].goto[c];
-                    self.states[u].fail = ufail;
-                    let ufail_out = self.states[ufail].out.clone();
-                    self.states[u].out.extend(ufail_out);
+                    let ufail = self.states[v as usize].goto[c];
+                    self.states[u as usize].fail = ufail;
+                    let ufail_out = self.states[ufail as usize].out.clone();
+                    self.states[u as usize].out.extend(ufail_out);
                 }
             }
         }
@@ -102,21 +254,7 @@ impl Automaton {
     fn add_state(&mut self, state: State) -> StateIdx {
         let i = self.states.len();
         self.states.push(state);
-        i
-    }
-
-    pub fn find(&self, s: &str) -> Vec<usize> {
-        let mut si = 1;
-        for &b in s.as_bytes() {
-            while self.states[si].goto[b as usize] == 0 {
-                si = self.states[si].fail;
-            }
-            si = self.states[si].goto[b as usize];
-            if !self.states[si].out.is_empty() {
-                return self.states[si].out.clone();
-            }
-        }
-        vec![]
+        i as StateIdx
     }
 }
 
@@ -128,15 +266,15 @@ impl State {
             goto: vec![0; 256],
         }
     }
-
-    fn goto(&self, b: u8) -> Option<StateIdx> {
-        let i = self.goto[b as usize];
-        if i == 0 { None } else { Some(i) }
-    }
-
 }
 
-impl fmt::Debug for Automaton {
+impl<S: Into<String>> FromIterator<S> for AcAutomaton {
+    fn from_iter<T>(it: T) -> AcAutomaton where T: IntoIterator<Item=S> {
+        AcAutomaton::new(it)
+    }
+}
+
+impl fmt::Debug for AcAutomaton {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         use std::iter::repeat;
 
@@ -175,13 +313,181 @@ impl fmt::Debug for State {
 
 #[cfg(test)]
 mod tests {
-    use super::Builder;
+    use super::{AcAutomaton, Match};
+
+    fn naive_find<S: Into<String>>(xs: Vec<S>, haystack: S) -> Vec<Match> {
+        let needles: Vec<String> = xs.into_iter().map(Into::into).collect();
+        let haystack: String = haystack.into();
+        let mut matches = vec![];
+        for hi in 0..haystack.len() {
+            let rest = &haystack.as_bytes()[hi..];
+            for (pati, needle) in needles.iter().enumerate() {
+                let needle = needle.as_bytes();
+                if needle.len() > rest.len() {
+                    continue;
+                }
+                if needle == rest {
+                    matches.push(Match {
+                        pati: pati,
+                        start: hi,
+                        end: hi + needle.len(),
+                    });
+                }
+            }
+        }
+        matches
+    }
+
+    fn aut_find<S: Into<String>>(xs: Vec<S>, haystack: S) -> Vec<Match> {
+        AcAutomaton::new(xs).find(&haystack.into()).collect()
+    }
+
+    fn aut_findo<S: Into<String>>(xs: Vec<S>, haystack: S) -> Vec<Match> {
+        AcAutomaton::new(xs).find_overlapping(&haystack.into()).collect()
+    }
+
+    #[test]
+    fn one_pattern_one_match() {
+        let ns = vec!["a"];
+        let hay = "za";
+        assert_eq!(aut_find(ns, hay), vec![
+            Match { pati: 0, start: 1, end: 2 },
+        ]);
+    }
+
+    #[test]
+    fn one_pattern_many_match() {
+        let ns = vec!["a"];
+        let hay = "zazazzzza";
+        assert_eq!(aut_find(ns, hay), vec![
+            Match { pati: 0, start: 1, end: 2 },
+            Match { pati: 0, start: 3, end: 4 },
+            Match { pati: 0, start: 8, end: 9 },
+        ]);
+    }
+
+    #[test]
+    fn one_longer_pattern_one_match() {
+        let ns = vec!["abc"];
+        let hay = "zazabcz";
+        assert_eq!(aut_find(ns, hay), vec![
+            Match { pati: 0, start: 3, end: 6 },
+        ]);
+    }
+
+    #[test]
+    fn one_longer_pattern_many_match() {
+        let ns = vec!["abc"];
+        let hay = "zazabczzzzazzzabc";
+        assert_eq!(aut_find(ns, hay), vec![
+            Match { pati: 0, start: 3, end: 6 },
+            Match { pati: 0, start: 14, end: 17 },
+        ]);
+    }
+
+    #[test]
+    fn many_pattern_one_match() {
+        let ns = vec!["a", "b"];
+        let hay = "zb";
+        assert_eq!(aut_find(ns, hay), vec![
+            Match { pati: 1, start: 1, end: 2 },
+        ]);
+    }
+
+    #[test]
+    fn many_pattern_many_match() {
+        let ns = vec!["a", "b"];
+        let hay = "zbzazzzzb";
+        assert_eq!(aut_find(ns, hay), vec![
+            Match { pati: 1, start: 1, end: 2 },
+            Match { pati: 0, start: 3, end: 4 },
+            Match { pati: 1, start: 8, end: 9 },
+        ]);
+    }
+
+    #[test]
+    fn many_longer_pattern_one_match() {
+        let ns = vec!["abc", "xyz"];
+        let hay = "zazxyzz";
+        assert_eq!(aut_find(ns, hay), vec![
+            Match { pati: 1, start: 3, end: 6 },
+        ]);
+    }
+
+    #[test]
+    fn many_longer_pattern_many_match() {
+        let ns = vec!["abc", "xyz"];
+        let hay = "zazxyzzzzzazzzabcxyz";
+        assert_eq!(aut_find(ns, hay), vec![
+            Match { pati: 1, start: 3, end: 6 },
+            Match { pati: 0, start: 14, end: 17 },
+            Match { pati: 1, start: 17, end: 20 },
+        ]);
+    }
+
+    #[test]
+    fn many_longer_pattern_overlap_one_match() {
+        let ns = vec!["abc", "bc"];
+        let hay = "zazabcz";
+        assert_eq!(aut_findo(ns, hay), vec![
+            Match { pati: 0, start: 3, end: 6 },
+            Match { pati: 1, start: 4, end: 6 },
+        ]);
+    }
+
+    #[test]
+    fn many_longer_pattern_overlap_one_match_reverse() {
+        let ns = vec!["abc", "bc"];
+        println!("{:?}", AcAutomaton::new(ns.clone()));
+        let hay = "xbc";
+        assert_eq!(aut_findo(ns, hay), vec![
+            Match { pati: 1, start: 1, end: 3 },
+        ]);
+    }
+
+
+    #[test]
+    fn many_longer_pattern_overlap_many_match() {
+        let ns = vec!["abc", "bc", "c"];
+        let hay = "zzzabczzzbczzzc";
+        assert_eq!(aut_findo(ns, hay), vec![
+            Match { pati: 0, start: 3, end: 6 },
+            Match { pati: 1, start: 4, end: 6 },
+            Match { pati: 2, start: 5, end: 6 },
+            Match { pati: 1, start: 9, end: 11 },
+            Match { pati: 2, start: 10, end: 11 },
+            Match { pati: 2, start: 14, end: 15 },
+        ]);
+    }
+
+    #[test]
+    fn many_longer_pattern_overlap_many_match_reverse() {
+        let ns = vec!["abc", "bc", "c"];
+        let hay = "zzzczzzbczzzabc";
+        assert_eq!(aut_findo(ns, hay), vec![
+            Match { pati: 2, start: 3, end: 4 },
+            Match { pati: 1, start: 7, end: 9 },
+            Match { pati: 2, start: 8, end: 9 },
+            Match { pati: 0, start: 12, end: 15 },
+            Match { pati: 1, start: 13, end: 15 },
+            Match { pati: 2, start: 14, end: 15 },
+        ]);
+    }
+
+    #[test]
+    fn weird_dna() {
+        let ns = vec!["tttacccc"];
+        println!("{:?}", AcAutomaton::new(ns.clone()));
+        let hay = "ttttacccc";
+        assert_eq!(aut_find(ns, hay), vec![
+            Match { pati: 0, start: 1, end: 9 },
+        ]);
+    }
 
     #[test]
     fn scratch() {
-        let aut =
-            Builder::new().add("he").add("she").add("his").add("hers").build();
+        let aut = AcAutomaton::new(vec!["he", "she", "his", "hers"]);
         println!("{:?}", aut);
-        println!("{:?}", aut.find("but she said"));
+        println!("{:?}", aut.find("but she said").next());
     }
 }
