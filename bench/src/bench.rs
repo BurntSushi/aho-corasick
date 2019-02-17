e8493baf54d0685d35c2e0a3bf4104717bdf0bd2
Andrew Gallant <jamslam@gmail.com>
2019-02-17 18:43:15 -0500

    rewrite the entire crate
    This commit introduces a ground-up rewrite of the entire crate. Most or
    all use cases served by `aho-corasick 0.6` should be served by this
    rewrite as well. Pretty much everything has been improved. The API is
    simpler, and much more flexible with many new configuration knobs for
    controlling the space-vs-time tradeoffs of Aho-Corasick automatons. In
    particular, there are several tunable optimizations for controlling
    space usage such as state ID representation and byte classes.
    
    The API is simpler in that there is now just one type that encapsulates
    everything: `AhoCorasick`.
    
    Support for streams has been improved quite a bit, with new APIs for
    stream search & replace.
    
    Test and benchmark coverage has increased quite a bit.
    
    This also fixes a subtle but important bug: empty patterns are now
    handled correctly. Previously, they could never match, but now they can
    match at any position.
    
    Finally, I believe this is now the only Aho-Corasick implementation to
    support leftmost-first and leftmost-longest semantics by using what I
    think is a novel alteration to the Aho-Corasick construction algorithm.
    I surveyed some other implementations, and there are a few Java
    libraries that support leftmost-longest match semantics, but they
    implement it by adding a sliding queue at search time. I also looked
    into Perl's regex implementation which has an Aho-Corasick optimization
    for `foo|bar|baz|...|quux` style regexes, and therefore must somehow
    implement leftmost-first semantics. The code is a bit hard to grok, but
    it looks like this is being handled at search time as opposed to baking
    it into the automaton.
    
    Fixes #18, Fixes #19, Fixes #26, Closes #34

--- bench/src/bench.rs
+++ bench/src/bench.rs
@@ -4,7 +4,7 @@ extern crate criterion;
 
 use std::time::Duration;
 
-use aho_corasick::{Automaton, AcAutomaton};
+use aho_corasick::{AhoCorasick, AhoCorasickBuilder, MatchKind};
 use criterion::{Bencher, Benchmark, Criterion, Throughput};
 
 mod build;
@@ -37,16 +37,90 @@ fn define_aho_corasick<B: AsRef<[u8]>>(
 
     let haystack = corpus.to_vec();
     let name = format!("nfa/{}", bench_name);
-    let aut = AcAutomaton::new(patterns.clone());
+    let aut = AhoCorasick::new(patterns.clone());
     define(c, group_name, &name, corpus, move |b| {
-        b.iter(|| assert_eq!(count, aut.find(&haystack).count()));
+        b.iter(|| assert_eq!(count, aut.find_iter(&haystack).count()));
     });
 
     let haystack = corpus.to_vec();
     let name = format!("dfa/{}", bench_name);
-    let aut = AcAutomaton::new(patterns).into_full();
+    let aut = AhoCorasickBuilder::new()
+        .dfa(true)
+        .build(patterns.clone());
     define(c, group_name, &name, corpus, move |b| {
-        b.iter(|| assert_eq!(count, aut.find(&haystack).count()));
+        b.iter(|| assert_eq!(count, aut.find_iter(&haystack).count()));
+    });
+}
+
+/// Define a benchmark that tests the different combinations of Aho-Corasick
+/// DFAs. e.g., With and without byte classes and premultiplied state ids.
+fn define_aho_corasick_dfa<B, F>(
+    c: &mut Criterion,
+    group_name: &str,
+    bench_name: &str,
+    corpus: &[u8],
+    kind: MatchKind,
+    count: usize,
+    patterns: Vec<B>,
+    find_count: F,
+) where B: AsRef<[u8]>,
+        F: 'static + Clone + Fn(&AhoCorasick, &[u8]) -> usize
+{
+    let patterns: Vec<Vec<u8>> = patterns
+        .into_iter()
+        .map(|b| b.as_ref().to_vec())
+        .collect();
+
+    let counter = find_count.clone();
+    let haystack = corpus.to_vec();
+    let name = format!("dfa/byteclass-premultiply/{}", bench_name);
+    let aut = AhoCorasickBuilder::new()
+        .match_kind(kind)
+        .dfa(true)
+        .byte_classes(true)
+        .premultiply(true)
+        .build(patterns.clone());
+    define(c, group_name, &name, corpus, move |b| {
+        b.iter(|| assert_eq!(count, counter(&aut, &haystack)))
+    });
+
+    let counter = find_count.clone();
+    let haystack = corpus.to_vec();
+    let name = format!("dfa/nobyteclass-premultiply/{}", bench_name);
+    let aut = AhoCorasickBuilder::new()
+        .match_kind(kind)
+        .dfa(true)
+        .byte_classes(false)
+        .premultiply(true)
+        .build(patterns.clone());
+    define(c, group_name, &name, corpus, move |b| {
+        b.iter(|| assert_eq!(count, counter(&aut, &haystack)))
+    });
+
+    let counter = find_count.clone();
+    let haystack = corpus.to_vec();
+    let name = format!("dfa/byteclass-nopremultiply/{}", bench_name);
+    let aut = AhoCorasickBuilder::new()
+        .match_kind(kind)
+        .dfa(true)
+        .byte_classes(true)
+        .premultiply(false)
+        .build(patterns.clone());
+    define(c, group_name, &name, corpus, move |b| {
+        b.iter(|| assert_eq!(count, counter(&aut, &haystack)))
+    });
+
+    let counter = find_count.clone();
+    let haystack = corpus.to_vec();
+    let name = format!("dfa/nobyteclass-nopremultiply/{}", bench_name);
+    let aut = AhoCorasickBuilder::new()
+        .match_kind(kind)
+        .dfa(true)
+        .byte_classes(false)
+        .premultiply(false)
+        .build(patterns.clone());
+    define(c, group_name, &name, corpus, move |b| {
+        b.iter(|| assert_eq!(count, counter(&aut, &haystack)))
     });
 }
 
@@ -65,7 +139,7 @@ fn define(
     let benchmark = Benchmark::new(bench_name, bench)
         .throughput(tput)
         .warm_up_time(Duration::from_millis(500))
-        .measurement_time(Duration::from_secs(2));
+        .measurement_time(Duration::from_secs(3));
     c.bench(group_name, benchmark);
 }
 
